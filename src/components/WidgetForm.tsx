52c778b2804619d55686a424aa8311d02c03c028
saviojks <saviooribe@gmail.com>
2022-07-12 14:27:38 -0300

    init

---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,15 @@
+export function WidgetForm() {
+    return (
+        <div className="BG-zinc-980 p-4 relative rounded-2xl ab-4 flex-color items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            <header>
+                <span className="text-xl leading-6">Deixe seu Feedback</span>
+            </header>
+
+            <p>hello world</p>
+
+            <footer className="text-xs text-neutral-400">
+                Feito com ❤ pela Rocketeseat<a className="underline underline-offset-2" href="//rocketseat.com.br">Rocketseat</a>
+            </footer>
+        </div>
+    );
+}
